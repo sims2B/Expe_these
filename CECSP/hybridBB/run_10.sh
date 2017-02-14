a0b5c1b346ba8e083a922155714b60c23d307792
sims2B <mnattaf@live.fr>
2017-02-14 12:15:35 +0100

    rr

---
+++ CECSP/hybridBB/run_10.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+perl -w BB_expl_test.pl ~/instance_CECSP/instance_CECSP_function/piecewise_linear_function/CPAIOR/inst_lin_id/inst10/ ~/Dev2016/CECSP/resultats_2017/CPAIOR/res_BB_Lid_TL7200_inst10;
+perl -w BB_expl_test.pl ~/instance_CECSP/instance_CECSP_function/piecewise_linear_function/CPAIOR/inst_lin_v1/inst10/ ~/Dev2016/CECSP/resultats_2017/CPAIOR/res_BB_Lv1_TL7200_inst10;
+perl -w BB_expl_test.pl ~/instance_CECSP/instance_CECSP_function/piecewise_linear_function/CPAIOR/inst_lin_v2/inst10/ ~/Dev2016/CECSP/resultats_2017/CPAIOR/res_BB_Lv2_TL7200_inst10;
+#perl -w BB_expl_test.pl ~/instance_CECSP/instance_CECSP_function/piecewise_linear_function/CPAIOR/inst_lin_v3/inst10/ ~/Dev2016/CECSP/resultats_2017/CPAIOR/res_BB_PLWv3_TL7200_inst20
